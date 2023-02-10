9acda839f1e54ae8898f381f9a02951672a017da
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2023-02-10 10:19:55 -0500

    Final commit

--- api/src/routes/post/routesPostDog.js
+++ api/src/routes/post/routesPostDog.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
     const {name, min_weight, max_weight, min_height, max_height, life_span, temperament, image} = req.body;
     try {
         if(!name || !min_weight || !max_weight || !min_height || !max_height || !life_span || !temperament || !image){
-            return res.status(404).json("Missing data to create Dog");
+            return res.status(404).send("Missing data to create Dog");
         }
         const newDog = await postDog(name, min_weight, max_weight, min_height, max_height, life_span, temperament, image);
         res.status(200).send(newDog);
